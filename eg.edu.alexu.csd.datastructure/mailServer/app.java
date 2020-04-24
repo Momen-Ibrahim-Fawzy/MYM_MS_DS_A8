33b26fd1adabf448295d75e65f92a8872f03ef23
Marwan Abougabal <54478282+99marwan@users.noreply.github.com>
2020-04-24 23:42:21 +0200

    Update app.java

--- eg.edu.alexu.csd.datastructure/mailServer/app.java
+++ eg.edu.alexu.csd.datastructure/mailServer/app.java
@@ -1,4 +1,55 @@
 package eg.edu.alexu.csd.datastructure.mailServer;
 
-public class app {
+import eg.edu.alexu.csd.datastructure.linkedList.ILinkedList;
+
+public class app implements IApp {
+    public folder folder;
+
+    public app(){
+
+        folder=new folder();
+        folder.setPath("System");
+    }
+
+    @Override
+    public boolean signin(String email, String password) {
+        boolean check1=folder.check_exist_username(email);
+        boolean check2=folder.check_password(email,password);
+        if(check1==true && check2==true)
+            return true;
+        return false;
+    }
+
+    @Override
+    public boolean signup(contact contact) {
+        if(folder.check_exist_username(contact.getFirstEmail()))
+            return  false;
+
+        return true;
+    }
+
+    @Override
+    public void setViewingOptions(IFolder folder, IFilter filter, ISort sort) {
+
+    }
+
+    @Override
+    public IMail[] listEmails(int page) {
+        return new IMail[0];
+    }
+
+    @Override
+    public void deleteEmails(ILinkedList mails) {
+
+    }
+
+    @Override
+    public void moveEmails(ILinkedList mails, IFolder des) {
+
+    }
+
+    @Override
+    public boolean compose(IMail email) {
+        return false;
+    }
 }
